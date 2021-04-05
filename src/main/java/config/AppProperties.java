865a29a99bba916a50be437b3012dd783e263d30
meffcio <meffcio@users.noreply.github.com>
2021-04-06 00:58:39 +0200

    Colors refactor

--- src/main/java/config/AppProperties.java
+++ src/main/java/config/AppProperties.java
@@ -2,25 +2,27 @@ package config;
 
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.hexworks.zircon.api.component.ColorTheme;
 import resources.Colors;
 import translator.TranslatorService;
 
 import java.io.*;
+import java.util.Locale;
 import java.util.Properties;
 
 
+@Slf4j
 @Getter
 public class AppProperties {
     //TUTAJ TWORZYC PROPERTASY
-    private static Colors colors = new Colors();
     String test="";
     String language="";
     String translated="";
     String login="";
     String password="";
     String version="";
-    ColorTheme theme = colors.getCapitol();
+    ColorTheme theme;
     InputStream inputStream;
     BufferedReader bufferedReader;
     private static TranslatorService translatorService = new TranslatorService();
@@ -38,36 +40,12 @@ public class AppProperties {
             language = translatorService.translateToTile(prop.getProperty("language"));
             translated = translatorService.translateToTile("zazółć gęślą jaźń");
             version = prop.getProperty("version");
-            switch (prop.getProperty("theme")){
-                case "capitol":
-                    break;
-                case "mojave":
-                    theme = colors.getMojave();
-                    break;
-                case "zgon":
-                    theme = colors.getZgon();
-                    break;
-                case "shpery":
-                    theme = colors.getShpery();
-                    break;
-                case "cyfroni":
-                    theme = colors.getCyfroni();
-                    break;
-                case "arystokracja":
-                    theme = colors.getArystokracja();
-                    break;
-                case "kosmodrom":
-                    theme = colors.getKosmodrom();
-                    break;
-                case "system":
-                    theme = colors.getSystem();
-                    break;
-                case "ammo":
-                    theme = colors.getAMMO();
-                    break;
-                case "cyberpunk":
-                    theme = colors.getCYBERPUNK();
-                    break;
+            String themeProperty = prop.getProperty("theme");
+            try {
+                theme = Colors.valueOf(themeProperty.toUpperCase(Locale.ROOT)).getColorTheme();
+            } catch (IllegalArgumentException ex) {
+                log.error("Theme {} unrecognized, setting to default.", themeProperty);
+                theme = Colors.CAPITOL.getColorTheme();
             }
         } catch (Exception e) {
             System.out.println("Exception: " + e);
--- src/main/java/resources/Colors.java
+++ src/main/java/resources/Colors.java
@@ -1,135 +1,98 @@
 package resources;
 
-
+import lombok.Getter;
 import org.hexworks.zircon.api.ColorThemes;
 import org.hexworks.zircon.api.color.TileColor;
 import org.hexworks.zircon.api.component.ColorTheme;
 
+@SuppressWarnings("unused")
+public enum Colors {
 
-public class Colors {
-
-    ColorTheme capitol = ColorThemes.newBuilder()
+    CAPITOL(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#57c183"))
             .withPrimaryForegroundColor(TileColor.fromString("#57c183"))
             .withSecondaryForegroundColor(TileColor.fromString("#57c183"))
             .withPrimaryBackgroundColor(TileColor.fromString("#103621"))
             .withSecondaryBackgroundColor(TileColor.fromString("#103621"))
-            .build();
+            .build()),
 
-    ColorTheme mojave = ColorThemes.newBuilder()
+    MOJAVE(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#a48e54"))
             .withPrimaryForegroundColor(TileColor.fromString("#a48e54"))
             .withSecondaryForegroundColor(TileColor.fromString("#a48e54"))
             .withPrimaryBackgroundColor(TileColor.fromString("#000000"))
             .withSecondaryBackgroundColor(TileColor.fromString("#000000"))
-            .build();
+            .build()),
 
-    ColorTheme zgon = ColorThemes.newBuilder()
+    ZGON(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#ffffff"))
             .withPrimaryForegroundColor(TileColor.fromString("#ffffff"))
             .withSecondaryForegroundColor(TileColor.fromString("#ffffff"))
             .withPrimaryBackgroundColor(TileColor.fromString("#283891"))
             .withSecondaryBackgroundColor(TileColor.fromString("#283891"))
-            .build();
+            .build()),
 
-    ColorTheme shpery = ColorThemes.newBuilder()
+    SHPERY(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#ffffff"))
             .withPrimaryForegroundColor(TileColor.fromString("#ffffff"))
             .withSecondaryForegroundColor(TileColor.fromString("#ffffff"))
             .withPrimaryBackgroundColor(TileColor.fromString("#000000"))
             .withSecondaryBackgroundColor(TileColor.fromString("#000000"))
-            .build();
+            .build()),
 
-    ColorTheme cyfroni = ColorThemes.newBuilder()
+    CYFRONI(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#000000"))
             .withPrimaryForegroundColor(TileColor.fromString("#000000"))
             .withSecondaryForegroundColor(TileColor.fromString("#000000"))
             .withPrimaryBackgroundColor(TileColor.fromString("#ffffff"))
             .withSecondaryBackgroundColor(TileColor.fromString("#ffffff"))
-            .build();
+            .build()),
 
-    ColorTheme arystokracja = ColorThemes.newBuilder()
+    ARYSTOKRACJA(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#540113"))
             .withPrimaryForegroundColor(TileColor.fromString("#aaaca9"))
             .withSecondaryForegroundColor(TileColor.fromString("#aaaca9"))
             .withPrimaryBackgroundColor(TileColor.fromString("#540113"))
             .withSecondaryBackgroundColor(TileColor.fromString("#540113"))
-            .build();
+            .build()),
 
-    ColorTheme kosmodrom = ColorThemes.newBuilder()
+    KOSMODROM(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#6e91c1"))
             .withPrimaryForegroundColor(TileColor.fromString("#000000"))
             .withSecondaryForegroundColor(TileColor.fromString("#000000"))
             .withPrimaryBackgroundColor(TileColor.fromString("#6e91c1"))
             .withSecondaryBackgroundColor(TileColor.fromString("#6e91c1"))
-            .build();
+            .build()),
 
-    ColorTheme system = ColorThemes.newBuilder()
+    SYSTEM(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#57c0a1"))
             .withPrimaryForegroundColor(TileColor.fromString("#57c0a1"))
             .withSecondaryForegroundColor(TileColor.fromString("#57c0a1"))
             .withPrimaryBackgroundColor(TileColor.fromString("#004938"))
             .withSecondaryBackgroundColor(TileColor.fromString("#004938"))
-            .build();
+            .build()),
 
-    ColorTheme AMMO = ColorThemes.newBuilder()
+    AMMO(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#eeffcc"))
             .withPrimaryForegroundColor(TileColor.fromString("#bedc7f"))
             .withSecondaryForegroundColor(TileColor.fromString("#4d8061"))
             .withPrimaryBackgroundColor(TileColor.fromString("#112318"))
             .withSecondaryBackgroundColor(TileColor.fromString("#040c06"))
-            .build();
+            .build()),
 
-    ColorTheme CYBERPUNK = ColorThemes.newBuilder()
+    CYBERPUNK(ColorThemes.newBuilder()
             .withAccentColor(TileColor.fromString("#61D6C4"))
             .withPrimaryForegroundColor(TileColor.fromString("#71918C"))
             .withSecondaryForegroundColor(TileColor.fromString("#3D615F"))
             .withPrimaryBackgroundColor(TileColor.fromString("#25343B"))
             .withSecondaryBackgroundColor(TileColor.fromString("#212429"))
-            .build();
+            .build());
 
-
-
-
-    public ColorTheme getCapitol() {
-        return capitol;
+    Colors(ColorTheme colorTheme) {
+        this.colorTheme = colorTheme;
     }
 
-    public ColorTheme getMojave() {
-        return mojave;
-    }
-
-    public ColorTheme getZgon() {
-        return zgon;
-    }
-
-    public ColorTheme getShpery() {
-        return shpery;
-    }
-
-    public ColorTheme getCyfroni() {
-        return cyfroni;
-    }
-
-    public ColorTheme getArystokracja() {
-        return arystokracja;
-    }
-
-    public ColorTheme getKosmodrom() {
-        return kosmodrom;
-    }
-
-    public ColorTheme getSystem() {
-        return system;
-    }
-
-    public ColorTheme getAMMO() {
-        return AMMO;
-    }
-
-    public ColorTheme getCYBERPUNK() {
-        return CYBERPUNK;
-    }
-
+    @Getter
+    private final ColorTheme colorTheme;
 
 }
